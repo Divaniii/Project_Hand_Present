a006efcfafc7a58b133b24dda91774f716b766c3
Divaniii <143288807+Divaniii@users.noreply.github.com>
2025-11-09 22:36:49 +0200

    Add drawing and erase functionality to hand gesture presentation

--- Project_Hand_Present/main.py
+++ Project_Hand_Present/main.py
@@ -2,6 +2,8 @@ import os
 
 import cv2
 
+import numpy as np
+
 from cvzone.HandTrackingModule import HandDetector
 
 #Variables
@@ -24,6 +26,9 @@ gestureThreshold = 300
 buttonPress = False
 buttonCounter = 0
 buttonDelay = 30
+annotations = [[]] # List within a list
+annotationNumber = -1
+annotationStart = False
 
 #Hand Detector
 detector = HandDetector(detectionCon=0.8, maxHands=1) #0.8: confidence that it is a hand
@@ -43,6 +48,26 @@ while True:
         hand = hands[0]
         fingers = detector.fingersUp(hand) #count how many fingers are up
         cx, cy = hand['center']
+
+        lmlist = hand['lmList']
+
+        # Map from webcam coordinates to slide coordinates
+        index_x = lmlist[8][0]  # Index finger tip X coordinate in webcam
+        index_y = lmlist[8][1]  # Index finger tip Y coordinate in webcam
+
+        # Map webcam coordinates (0 to width/height) to slide coordinates (0 to slide_w/slide_h)
+        # Added padding to make movement easier at edges
+        padding = 50  # pixels of padding for easier movement at edges
+
+        # Map X: from webcam width to slide width with padding
+        slide_x = int(np.interp(index_x, [padding, width - padding], [0, width]))
+        slide_x = max(0, min(slide_x, width))  # Constrain to slide boundaries
+
+        # Map Y: from webcam height to slide height with padding
+        slide_y = int(np.interp(index_y, [padding, height - padding], [0, height]))
+        slide_y = max(0, min(slide_y, height))  # Constrain to slide boundaries
+
+        indexfinger = (slide_x, slide_y)
         print(fingers)
 
         if cy <= gestureThreshold: #if hand is at hte height of the face
@@ -50,13 +75,45 @@ while True:
                 print("Left")
                 if imgNumber>0:
                     buttonPress = True
+                    annotations = [[]]  # List within a list
+                    annotationNumber = 0
+                    annotationStart = False
                     imgNumber -= 1
 
             if fingers == [0,0,0,0,1]: #Gesture 2 : Right
                 print("Right")
                 if imgNumber < len(pathImages):
                     buttonPress = True
+                    annotations = [[]]  # List within a list
+                    annotationNumber = 0
+                    annotationStart = False
                     imgNumber += 1
+
+            if fingers == [0,0,0,0,0]:
+                break
+
+        if fingers == [0,1,1,0,0]: #Gesture 3 : Pointer
+            cv2.circle(imgCurrentSlide, indexfinger, 12, (0,0,255), cv2.FILLED)
+            annotationStart = False
+
+        if fingers == [0,1,0,0,0]: #Gesture 4 : Draw
+            cv2.circle(imgCurrentSlide, indexfinger, 12, (0,0,255), cv2.FILLED)
+            if annotationStart is False:
+                annotationStart = True
+                annotationNumber += 1
+                annotations.append([]) # without this - no list to call
+            annotations[annotationNumber].append(indexfinger)
+
+        else:
+            annotationStart = False
+
+        if fingers == [0,1,1,1,0]:
+            if annotations:
+                if annotationNumber >= 0:
+                    annotations.pop(-1)#remove the last drawing
+                    annotationNumber -= 1
+                    buttonPress = True
+
 
 #Button Pressed Iterations
     if buttonPress:
@@ -64,6 +121,11 @@ while True:
         if buttonCounter > buttonDelay:
             buttonCounter = 0
             buttonPress = False
+
+    for i in range((len(annotations))):
+        for j in range(len(annotations[i])): # loop thorugh all of the points
+            if j!=0:
+                cv2.line(imgCurrentSlide, annotations[i][j-1], annotations[i][j], (0,0,200), 12)
 
 #Addding webcam image on slide
     imgSmall = cv2.resize(img, (ws, hs))
